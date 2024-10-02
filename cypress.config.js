f0d3221bb282e2d516d9fdd2bdee12edef04ac15
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-10-03 01:41:34 +0200

    devops spring 2 intento 5

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "cypress";
 
+require('dotenv').config();
+
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.env.VITE_MIAPI = process.env.VITE_MIAPI;
+      config.env.VITE_NAME = process.env.VITE_NAME;
+      config.env.VITE_PASSWORD = process.env.VITE_PASSWORD;
+      
+      return config;
+    },
+  },
+};
+
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -1,46 +1,46 @@
 describe('The Home Page', () => {
-    beforeEach(() => {
-      cy.clearLocalStorage(); 
-    });
-    it('It obtains the API token', () => {
-      const MIAPI = process.env.VITE_MIAPI;
-      const name = process.env.VITE_NAME;
-      const password = process.env.VITE_PASSWORD; 
+  beforeEach(() => {
+    cy.clearLocalStorage(); 
+  });
 
+  it('It obtains the API token', () => {
+    const MIAPI = Cypress.env('VITE_MIAPI');
+    const name = Cypress.env('VITE_NAME');
+    const password = Cypress.env('VITE_PASSWORD'); 
 
-      cy.request({
-        method: 'POST',
-        url: `${MIAPI}/login`, 
-        body: {
-          name,
-          password
-        }
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        const token = response.body.token;
-        
-        cy.log('Token recibido:', token);
-        cy.window().then((window) => {
-          window.localStorage.setItem('authorization', token);
-        });
+    cy.request({
+      method: 'POST',
+      url: `${MIAPI}/login`, 
+      body: {
+        name,
+        password
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      const token = response.body.token;
+      
+      cy.log('Token recibido:', token);
+      cy.window().then((window) => {
+        window.localStorage.setItem('authorization', token);
       });
     });
+  });
 
-    it('successfully loads', () => {
-      cy.visit('/') 
-      cy.url().should('include', '/login')      
+  it('successfully loads', () => {
+    cy.visit('/') 
+    cy.url().should('include', '/login');      
 
-      const email = process.env('VITE_NAME');
-      const pass = process.env('VITE_PASSWORD');
+    const email = Cypress.env('VITE_NAME');
+    const pass = Cypress.env('VITE_PASSWORD');
 
-      cy.get('input[name=name]').type(`${email}`)
-      cy.get('input[type=password]').type(`${pass}`)
-      cy.wait(2000);
-      cy.get('button[type=submit]').click()
-      cy.wait(10000);
+    cy.get('input[name=name]').type(`${email}`);
+    cy.get('input[type=password]').type(`${pass}`);
+    cy.wait(2000);
+    cy.get('button[type=submit]').click();
+    cy.wait(10000);
 
-      cy.url().should('eq', 'http://localhost:5173/home')
+    cy.url().should('eq', 'http://localhost:5173/home');
 
-      cy.get('[data-testid="navbar-burger"]').should('be.visible');
-    })
-  })
+    cy.get('[data-testid="navbar-burger"]').should('be.visible');
+  });
+});
